f46c1531a36300703e9f6bf77417a69648fd8b2f
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-04-04 23:57:43 +0800

    Added secondary button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimaryButtonComponent } from './primary-button/primary-button.component';
+import { SecondaryButtonComponent } from './secondary-button/secondary-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    SecondaryButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/secondary-button/secondary-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bl-secondary-button',
+  templateUrl: './secondary-button.component.html',
+  styleUrls: ['./secondary-button.component.scss']
+})
+export class SecondaryButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
